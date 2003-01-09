61091c8fe06de07d78c10ec70185632343eb2c1c
ctl <ctl>
2003-01-09 14:15:25 +0000

    Changed license GPL->LGPL

--- 3dm/src/tdm/lib/Node.java
+++ 3dm/src/tdm/lib/Node.java
@@ -1,20 +1,20 @@
-// $Id: Node.java,v 1.12 2003/01/09 13:38:45 ctl Exp $ D
+// $Id: Node.java,v 1.13 2003/01/09 14:15:26 ctl Exp $ D
 //
 // Copyright (c) 2001, Tancred Lindholm <ctl@cs.hut.fi>
 //
 // This file is part of 3DM.
 //
 // 3DM is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
 // (at your option) any later version.
 //
 // 3DM is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// GNU Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
+// You should have received a copy of the GNU Lesser General Public License
 // along with 3DM; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
